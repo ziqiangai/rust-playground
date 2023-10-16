bc3cd4ea7f7757382fbd330ce7bf1c314019d79d
ziqiang <1694392889@qq.com>
2023-10-16 13:36:17 +0800

    feat: add react redux demo

---
+++ js/redux/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  reset,
+  logIn,
+  logOut,
+} from "./actions";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  return (
+      <div className="App">
+        <h1>
+          Hello World <br /> A little Redux Project. YaaY!
+        </h1>
+        <h3>Counter</h3>
+        <h3>{counter}</h3>
+        <button onClick={() => dispatch(increment())}>Increase</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => dispatch(decrement())}>Decrease</button>
+
+      </div>
+  );
+}
+
+export default App;
